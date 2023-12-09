b71a54c048db0a4206d872a1b485785257a47ff2
weslenpy <weslenjhony@gmail.com>
2023-12-09 10:17:09 -0300

    fix

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ class Convert:
     def start(self):
         password = input("Digite a senha do arquivo pfx: ")
         
-        pfx = LoaderPFX(password=password)
-        
-        pfx_data = pfx.load_pfx()
-        pfx_decrypt = pfx.decrypt_pfx(pfx_data=pfx_data)
-        pfx_to_pem = pfx.generate_pem(decrypt_pfx=pfx_decrypt)
-        
-        pfx.save(*pfx_to_pem)        
-        
-        
+        try:
+            pfx = LoaderPFX(password=password)
+            
+            pfx_data = pfx.load_pfx()
+            pfx_decrypt = pfx.decrypt_pfx(pfx_data=pfx_data)
+            pfx_to_pem = pfx.generate_pem(decrypt_pfx=pfx_decrypt)
+            
+            pfx.save(*pfx_to_pem)        
+            
+        except:
+            print("Ooops, algo deu errado.")
         
         
 if __name__ == "__main__":
